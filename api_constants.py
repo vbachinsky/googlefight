5bf57c865f0f7f2377401ccd853659a96ac33d8c
vbachinsky <55666732+vbachinsky@users.noreply.github.com>
2019-10-07 05:08:49 +0300

    Battle anywhere with any warriors v 0.1 pre-Alpha

---
+++ api_constants.py
@@ -0,0 +1,3 @@
+KEYS = {'key':'AIzaSyDh5DQw2WWyCRsQ7xbxC2kKpfXO4comnS8'}
+
+URL = 'https://maps.googleapis.com/maps/api/geocode/json?'
---
+++ game_massages.py
@@ -0,0 +1,7 @@
+
+"""Basic messages"""
+
+HELLO = "Welcome to Battle!!!"
+WIN = "You win!"
+LOSE = "You lose!"
+WRONG = 'Somewhere you wrong :('
